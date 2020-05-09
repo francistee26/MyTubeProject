ee504edc012cc6e06d6ec16b7d75a93ccdcd28d7
Francis <francis.tholley@scranton.edu>
2020-05-08 20:29:53 -0400

    Employee Class

---
+++ OOP/Main.java
@@ -0,0 +1,17 @@
+import java.text.NumberFormat;
+
+public class Main {
+    public static void main(String[] args) {
+        var employee = new Employee();
+        employee.setBaseSalary(1);
+        employee.getBaseSalary();
+    
+        employee.setHourlyRate(0);
+        employee.getHourlyRate();
+
+        int wage = employee.calculateWage(10);
+
+        System.out.println(NumberFormat.getCurrencyInstance().format(wage));
+    }
+
+}
