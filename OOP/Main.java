cd438f297b1b34575d18460d76eb4d011126b98c
Francis <francis.tholley@scranton.edu>
2020-05-08 20:55:34 -0400

    work

--- OOP/Main.java
+++ OOP/Main.java
@@ -3,14 +3,9 @@ import java.text.NumberFormat;
 public class Main {
     public static void main(String[] args) {
          var employee = new Employee();
-        employee.setBaseSalary(1);
-        employee.getBaseSalary();
-    
-        employee.setHourlyRate(0);
-        employee.getHourlyRate();
-
+        employee.setBaseSalary(50_000);
+        employee.setHourlyRate(20);
         int wage = employee.calculateWage(10);
-
         System.out.println(NumberFormat.getCurrencyInstance().format(wage));
     }
 
